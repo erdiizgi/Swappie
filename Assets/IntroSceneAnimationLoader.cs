6b914571e9d3f163240c9f8d75b1b366b85da552
erdiizgi <erdi_izgi@hotmail.com.tr>
2016-01-05 23:50:44 +0100

    Intro Scene and Video added

---
+++ Assets/IntroSceneAnimationLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroSceneAnimationLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Handheld.PlayFullScreenMovie("swappie-intro-video.mp4", Color.black, FullScreenMovieControlMode.Hidden);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(int n)
+    {
+        Application.LoadLevel(n);
+    }
+}
